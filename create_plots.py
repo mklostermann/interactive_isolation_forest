d1e428ac03adc6a54c60a74c6c8435a3ac0931aa
Markus Klostermann <mklostermann@student.tugraz.at>
2020-10-12 22:33:09 +0200

    Change plots to use means

--- create_plots.py
+++ create_plots.py
@@ -28,7 +28,7 @@ def plot_anomalies_seen(dataset, algorithm_result_file, output_file):
     fig, ax = plt.subplots()
 
     for (algorithm, result_file) in algorithm_result_file:
-        data = np.loadtxt(result_file, dtype=int)
+        data = np.loadtxt(result_file)
         ax.plot(range(data.shape[0]), data, label=algorithm, alpha=0.8)
 
     ax.set_xlabel('Iterations')
@@ -43,19 +43,13 @@ def plot_anomalies_seen(dataset, algorithm_result_file, output_file):
 def main(datasets, algorithms):
     for dataset in datasets:
 
-        for i in range(1, 100): # TODO
-            algorithm_result_file = helper.get_result_files_for_algorithms(dataset, algorithms, f"auroc-{i}.csv")
-            if any(algorithm_result_file):
-                plot_auroc(dataset, algorithm_result_file, helper.get_plot_file(dataset, f"auroc-{i}"))
-            else:
-                break
+        algorithm_result_file = helper.get_result_files_for_algorithms(dataset, algorithms, f"auroc.csv")
+        if any(algorithm_result_file):
+            plot_auroc(dataset, algorithm_result_file, helper.get_plot_file(dataset, f"auroc"))
 
-        for i in range(1, 100):  # TODO
-            algorithm_result_file = helper.get_result_files_for_algorithms(dataset, algorithms, f"anomalies_seen-{i}.csv")
-            if any(algorithm_result_file):
-                plot_anomalies_seen(dataset, algorithm_result_file, helper.get_plot_file(dataset, f"anomalies_seen-{i}"))
-            else:
-                break
+        algorithm_result_file = helper.get_result_files_for_algorithms(dataset, algorithms, f"anomalies_seen.csv")
+        if any(algorithm_result_file):
+            plot_anomalies_seen(dataset, algorithm_result_file, helper.get_plot_file(dataset, f"anomalies_seen"))
 
 
 if __name__ == '__main__':

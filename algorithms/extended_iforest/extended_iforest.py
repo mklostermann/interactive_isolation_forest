Fix csv float precision, enable algorithms to work with new DatasetInfo, fix minor things

algorithms/extended_iforest/extended_iforest.py
@@ -5,20 +5,22 @@ import helper
 
 # Extended Isolation Forest from eif
 def detect(datasets, budget, runs):
-    for dataset in datasets:
-        results_dir = helper.get_results_dir(dataset, "extended_iforest")
+    for dataset_info in datasets:
+        results_dir = helper.get_results_dir(dataset_info.dataset, "extended_iforest")
         if not os.path.exists(results_dir):
             os.makedirs(results_dir)
 
-        for data_file in helper.get_all_data_files(dataset):
-            data, labels = helper.load_dataset(data_file)
+        data_file = dataset_info.get_data_file()
+        actual_runs = runs if dataset_info.downsampled_with_variations else runs * 10
+        data, labels = helper.load_dataset(data_file)
 
-            for run in range(1, runs + 1):
-                iforest = eif.iForest(data, ntrees=100, sample_size=256, ExtensionLevel=data.shape[1] - 1)
-                scores = iforest.compute_paths(data)
+        for run in range(1, actual_runs + 1):
+            samples = min(dataset_info.samples_count, 256)
+            iforest = eif.iForest(data, ntrees=100, sample_size=samples, ExtensionLevel=data.shape[1] - 1)
+            scores = iforest.compute_paths(data)
 
-                all_scores = np.vstack([scores] * (budget + 1))
-                helper.save_all_scores(all_scores, results_dir, data_file, run)
+            all_scores = np.vstack([scores] * (budget + 1))
+            helper.save_all_scores(all_scores, results_dir, data_file, run)
 
-                queried_instances = np.argsort(-scores)[np.arange(budget)]
-                helper.save_queried_instances(queried_instances, results_dir, data_file, run)
+            queried_instances = np.argsort(-scores)[np.arange(budget)]
+            helper.save_queried_instances(queried_instances, results_dir, data_file, run)

algorithms/iforest_eif/iforest_eif.py
@@ -3,22 +3,26 @@ import eif
 import numpy as np
 import helper
 
+
 # Isolation Forest as implemented by the eif (Extended Isolation Forest) module
 def detect(datasets, budget, runs):
-    for dataset in datasets:
-        results_dir = helper.get_results_dir(dataset, "iforest_eif")
+    for dataset_info in datasets:
+        results_dir = helper.get_results_dir(dataset_info.dataset, "iforest_eif")
+
         if not os.path.exists(results_dir):
             os.makedirs(results_dir)
 
-        for data_file in helper.get_all_data_files(dataset):
-            data, labels = helper.load_dataset(data_file)
+        data_file = dataset_info.get_data_file()
+        actual_runs = runs if dataset_info.downsampled_with_variations else runs * 10
+        data, labels = helper.load_dataset(data_file)
+        samples = min(dataset_info.samples_count, 256)
 
-            for run in range(1, runs + 1):
-                iforest = eif.iForest(data, ntrees=100, sample_size=256, ExtensionLevel=0)
-                scores = iforest.compute_paths(data)
+        for run in range(1, actual_runs + 1):
+            iforest = eif.iForest(data, ntrees=100, sample_size=samples, ExtensionLevel=0)
+            scores = iforest.compute_paths(data)
 
-                all_scores = np.vstack([scores] * (budget + 1))
-                helper.save_all_scores(all_scores, results_dir, data_file, run)
+            all_scores = np.vstack([scores] * (budget + 1))
+            helper.save_all_scores(all_scores, results_dir, data_file, run)
 
-                queried_instances = np.argsort(-scores)[np.arange(budget)]
-                helper.save_queried_instances(queried_instances, results_dir, data_file, run)
+            queried_instances = np.argsort(-scores)[np.arange(budget)]
+            helper.save_queried_instances(queried_instances, results_dir, data_file, run)

algorithms/iforest_sklearn/iforest_sklearn.py
@@ -6,22 +6,24 @@ import helper
 
 # Isolation Forest from scikit-learn
 def detect(datasets, budget, runs):
-    for dataset in datasets:
-        results_dir = helper.get_results_dir(dataset, "iforest_sklearn")
+    for dataset_info in datasets:
+        results_dir = helper.get_results_dir(dataset_info.dataset, "iforest_sklearn")
+
         if not os.path.exists(results_dir):
             os.makedirs(results_dir)
 
-        for data_file in helper.get_all_data_files(dataset):
-            data, labels = helper.load_dataset(data_file)
+        data_file = dataset_info.get_data_file()
+        actual_runs = runs if dataset_info.downsampled_with_variations else runs * 10
+        data, labels = helper.load_dataset(data_file)
 
-            for run in range(1, runs + 1):
-                iforest = sk.ensemble.IsolationForest(n_estimators=100, max_samples=256).fit(data)
+        for run in range(1, actual_runs + 1):
+            iforest = sk.ensemble.IsolationForest(n_estimators=100, max_samples=256).fit(data)
 
-                # Inversion required, as it returns the "opposite of the anomaly score defined in the original paper"
-                scores = -iforest.score_samples(data)
+            # Inversion required, as it returns the "opposite of the anomaly score defined in the original paper"
+            scores = -iforest.score_samples(data)
 
-                all_scores = np.vstack([scores] * (budget + 1))
-                helper.save_all_scores(all_scores, results_dir, data_file, run)
+            all_scores = np.vstack([scores] * (budget + 1))
+            helper.save_all_scores(all_scores, results_dir, data_file, run)
 
-                queried_instances = np.argsort(-scores)[np.arange(budget)]
-                helper.save_queried_instances(queried_instances, results_dir, data_file, run)
+            queried_instances = np.argsort(-scores)[np.arange(budget)]
+            helper.save_queried_instances(queried_instances, results_dir, data_file, run)

calc_metrics.py
@@ -10,55 +10,15 @@ from sklearn import metrics
 def calc_auroc(labels, input_file, output_file):
     all_scores = np.loadtxt(input_file, delimiter=',')
     auroc = [metrics.roc_auc_score(labels, all_scores[i, :]) for i in range(all_scores.shape[0])]
-    np.savetxt(output_file, auroc, fmt='%f', delimiter=',')
-    return auroc
+    np.savetxt(output_file, auroc, delimiter=',')
 
 
 def calc_anomalies_seen(labels, input_file, output_file):
     queried_instances = np.loadtxt(input_file, delimiter=',', dtype=int)
     anomalies_seen = np.concatenate(([0], np.cumsum(labels[queried_instances])))
     np.savetxt(output_file, anomalies_seen, fmt='%d', delimiter=',')
-    return anomalies_seen
 
 
-def calc_mean(data, output_file):
-    if not data:
-        return
-
-    mean = np.mean(data, axis=0)
-    stddev = np.std(data, axis=0)
-    np.savetxt(output_file, np.array([mean, stddev]), fmt='%f', delimiter=',')
-
-
-def collect_data(data):
-    collected_data = {"all": [], "le05": [], "le10": [], "le20": [], "orig": [], "02": [], "05": [], "10": [], "20": []}
-
-    for filename in data:
-        collected_data["all"].extend(data[filename])
-
-        outlier_rate, downsampled = helper.get_outlier_rate(filename)
-        if not downsampled:
-            collected_data["orig"].extend(data[filename])
-        if outlier_rate is not None:
-            if outlier_rate == 2:
-                collected_data["02"].extend(data[filename])
-            if outlier_rate == 5:
-                collected_data["05"].extend(data[filename])
-            if outlier_rate == 10:
-                collected_data["10"].extend(data[filename])
-            if outlier_rate == 20:
-                collected_data["20"].extend(data[filename])
-            if outlier_rate <= 5:
-                collected_data["le05"].extend(data[filename])
-            if outlier_rate <= 10:
-                collected_data["le10"].extend(data[filename])
-            if outlier_rate <= 20:
-                collected_data["le20"].extend(data[filename])
-
-    return collected_data
-
-
-# TODO: Create averaged scores
 def main(datasets, algorithms):
     logging.basicConfig(filename="log/calc_metrics.log", filemode='w',
                         format='%(asctime)s [%(threadName)s] %(message)s', level=logging.INFO)
@@ -73,10 +33,9 @@ def main(datasets, algorithms):
             logging.info(f"Working on algorithm {algorithm} on data set {dataset}")
 
             results_dir = helper.get_results_dir(dataset, algorithm)
+            metrics_dir = helper.get_metrics_dir(dataset, algorithm)
 
             if os.path.exists(results_dir):
-                auroc = {}
-                anomalies_seen = {}
 
                 for file in os.scandir(results_dir):
                     match = re.search(r"\A(all_scores|queried_instances)-(\w+)#(\d+)\.csv\Z", file.name)
@@ -89,40 +48,16 @@ def main(datasets, algorithms):
                             _, labels[filename] = helper.load_dataset(data_file)
 
                         if match.group(1) == "all_scores":
-                            value = calc_auroc(labels[filename], file.path,
-                                               os.path.join(results_dir,
-                                                            f"auroc-{match.group(2)}#{match.group(3)}.csv"))
-                            if filename not in auroc:
-                                auroc[filename] = []
-
-                            auroc[filename].append(value)
+                            calc_auroc(labels[filename], file.path,
+                                       os.path.join(metrics_dir,
+                                                    f"auroc-{match.group(2)}#{match.group(3)}.csv"))
 
                         if match.group(1) == "queried_instances":
-                            value = calc_anomalies_seen(labels[filename], file.path,
-                                                        os.path.join(results_dir,
-                                                                     f"anomalies_seen-{match.group(2)}#{match.group(3)}.csv"))
-
-                            if filename not in anomalies_seen:
-                                anomalies_seen[filename] = []
-
-                            anomalies_seen[filename].append(value)
+                            calc_anomalies_seen(labels[filename], file.path,
+                                                os.path.join(metrics_dir,
+                                                             f"anomalies_seen-{match.group(2)}#{match.group(3)}.csv"))
                     else:
                         logging.info(f"File {file.path} is ignored")
-
-                if any(auroc) and any(anomalies_seen):
-                    logging.info("Calculating mean (...) of AUROC")
-                    data = collect_data(auroc)
-
-                    for key in data:
-                        calc_mean(data[key], os.path.join(results_dir, f"auroc-{key}.csv"))
-
-                    logging.info("Calculating mean (...) of seen anomalies")
-                    data = collect_data(anomalies_seen)
-
-                    for key in data:
-                        calc_mean(data[key], os.path.join(results_dir, f"anomalies_seen-{key}.csv"))
-                else:
-                    logging.warning(f"No files to calculate mean of AUROC / seen anomalies for {dataset}/{algorithm}")
             else:
                 logging.warning(f"No result files for {dataset}/{algorithm}")
 

helper.py
@@ -108,6 +108,10 @@ def get_results_dir(dataset, algorithm=None):
     return os.path.abspath(os.path.join(os.getcwd(), f"results/{dataset}/{'' if algorithm is None else algorithm}"))
 
 
+def get_metrics_dir(dataset, algorithm=None):
+    return os.path.abspath(os.path.join(os.getcwd(), f"metrics/{dataset}/{'' if algorithm is None else algorithm}"))
+
+
 def get_plot_file(dataset, plotname):
     return os.path.abspath(os.path.join(os.getcwd(), f"plots/{dataset}-{plotname}.pdf"))
 
@@ -130,4 +134,4 @@ def save_queried_instances(queried_instances, results_dir, data_file, run):
 
 def save_all_scores(all_scores, results_dir, data_file, run):
     all_scores_file = os.path.join(results_dir, f"all_scores-{get_filename(data_file)}#{run}.csv")
-    np.savetxt(all_scores_file, all_scores, fmt='%f', delimiter=',')
+    np.savetxt(all_scores_file, all_scores, delimiter=',')
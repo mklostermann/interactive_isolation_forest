4139d0d21c420f1bcc438233d3ead1a1094a8858
Markus Klostermann <mklostermann@student.tugraz.at>
2023-06-23 21:43:03 +0200

    Add repeated IF

---
+++ algorithms/iforest_rep/iforest_rep.py
@@ -0,0 +1,34 @@
+import os
+import numpy as np
+from sklearn.ensemble import IsolationForest
+
+import helper
+
+# Unsupervised IF, repeatedly trained.
+def detect(datasets, budget, runs):
+    for dataset_info in datasets:
+        results_dir = helper.get_results_dir(dataset_info.dataset, "iforest_rep")
+
+        if not os.path.exists(results_dir):
+            os.makedirs(results_dir)
+
+        data_file = dataset_info.get_data_file()
+        actual_budget = dataset_info.outlier_count if budget <= 0 else budget
+        data, labels = helper.load_dataset(data_file)
+
+        for run in range(1, runs + 1):
+            queried_instances = []
+
+            for i in range(0, actual_budget):
+                iforest = IsolationForest(n_estimators=100, max_samples=256).fit(data)
+
+                # Inversion required, as it returns the "opposite of the anomaly score defined in the original paper"
+                scores = -iforest.score_samples(data)
+                for j in range(0, dataset_info.samples_count):
+                    queried = np.argsort(-scores)[j]
+                    if queried not in queried_instances:
+                        break
+
+                queried_instances.append(queried)
+
+            helper.save_queried_instances(queried_instances, results_dir, data_file, run)
